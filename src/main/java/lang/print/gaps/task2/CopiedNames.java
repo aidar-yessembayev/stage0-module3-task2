012fdd0cc7115e1b64f8513eb80a55b28152d763
Aidar Yessembayev <yessembayevaidar@gmail.com>
2022-07-27 22:32:22 +0600

    init

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,12 +3,12 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        int variable = 2000;
+        variable = 2000;
         int value = 1000;
-        int variable = 4000;
+        value = 4000;
         int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
-        int value = 90000;
+        value2 = 5000;
+        variable = 7000;
+        value = 90000;
     }
 }
